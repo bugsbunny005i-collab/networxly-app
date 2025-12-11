348b1b8ee302c2d4a4a6e2647a9bcde11f07116c
Dinesh Patil <bugsbunny005i@gmail.com>
2025-12-11 22:43:49 +0530

    Updated API URL

--- backend/server.js
+++ backend/server.js
@@ -65,7 +65,7 @@ app.use(hpp());
 
 // CORS Configuration (Allow Frontend)
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5000'], 
+    origin: ['http://localhost:5173', 'https://networxly-app.onrender.com'], 
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
